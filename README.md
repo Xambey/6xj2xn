0cb4af0ca8567ac10c23dda2eec90f1af583f089
Xambey <xambey@yandex.ru>
2020-08-25 16:35:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 6xj2xn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/6xj2xn)
---
+++ index.ts
@@ -0,0 +1,37 @@
+import { zip, of, from } from 'rxjs';
+import { map, tap, scan, delay, switchMap, flatMap, share, reduce } from 'rxjs/operators';
+
+let age$ = of<number>(27, 25, 29);
+let name$ = of<string>('Foo', 'Bar', 'Beer');
+let isDev$ = of<boolean>(true, true, false);
+
+// zip(age$, name$, isDev$).pipe(
+//   // tap(([age, name, isDev]) => { 
+//   //   console.log({ age, name, isDev });
+//   // }),
+// )
+// .subscribe(x => console.log(x));
+
+var arr = [1, 2, 3, 4];
+var t = from(arr);
+
+zip(
+  t.pipe(
+    delay(1000),
+    scan((g, v) => {;
+      return g.pipe(
+        flatMap(x => of(v)),
+        tap((f) => console.log('here ' + f))
+      );
+    }, of({})),
+    share(),
+    switchMap(x => x)
+  ),
+).subscribe(value => {
+  console.log(value);
+})
+
+// outputs
+// { age: 27, name: 'Foo', isDev: true }
+// { age: 25, name: 'Bar', isDev: true }
+// { age: 29, name: 'Beer', isDev: false }
