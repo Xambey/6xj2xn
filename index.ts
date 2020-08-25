f1c605fff2b93ee90ccb2914b90360055d549312
Xambey <xambey@yandex.ru>
2020-08-25 17:34:56 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -16,18 +16,45 @@ var arr = [1, 2, 3, 4];
 var t = from(arr);
 
 zip(
-  t.pipe(
-    scan((g, v) => {;
-        return g.pipe(
-          flatMap(x => of(v)),
-          tap((f) => console.log('here ' + f))
-        );
+  // t.pipe(
+  //   concatMap(x => of(x)),
+  //   scan((g, v) => {;
+  //       return g.pipe(
+  //         flatMap(x => of(v)),
+  //         tap((f) => console.log('here ' + f))
+  //       );
       
-      }, of({})
-    ),
-    concatMap(x => x)
+  //     }, of({})
+  //   ),
+  //   concatMap(x => x)
+  // ),
+  ...arr.map(x => of(x).pipe(
+
+    flatMap(x => {
+      const arr2 = from([5,6]);
+      return zip(
+        arr2.pipe(
+          tap(f => 'here arr2 before scan ' + f),
+          scan((g, v) => {;
+            return g.pipe(
+              flatMap(x => of(v)),
+              tap((f) => console.log('here arr 2' + f))
+            );
+           }, of({})
+          ),
+          flatMap(x => x)
+        )
+      )
+    }),
+    flatMap(x => x)
   ),
-).subscribe(value => {
+  )
+)
+.pipe(
+  tap(v => console.log('last pipe ')),
+  tap(v => console.log(v)),
+)
+.subscribe(value => {
   console.log(value);
 })
 
